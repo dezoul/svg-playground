5e1dbf282e0b273532739638804eacacec1fb51c
dezoul <aussieme@gmail.com>
2022-08-29 15:55:03 +0800

    works but to jumpy

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const arrows = document.querySelectorAll('.arrow-parent .guideDiv')
+const body = document.querySelector("body")
+const arrows = document.querySelectorAll('.arrow-parent .guideDiv');
 const bob = "well that work"
 const guideDiv = [...arrows]
-console.log(guideDiv)
+// console.log(guideDiv)
+
+// console.log(guideDiv)
 
 arrows.forEach(arrow => {
     var path = arrow.querySelector("path");
@@ -13,23 +16,29 @@ arrows.forEach(arrow => {
 })
 
 function checkSlide(e) {
-    guideDiv.forEach(sliderImage => {
-        const rect = sliderImage.getBoundingClientRect()
-        // half visable from the bottom point of each image 
-        const slideInAt = (window.scrollY + window.innerHeight) - rect.height / 2;
-        // the bottom of the image
-        const imageBottom = sliderImage.offsetTop + rect.height;
-        const isHalfShown = slideInAt > sliderImage.offsetTop;
-        const isNotScrolledPast = window.scrollY < imageBottom;
-        if (isHalfShown && isNotScrolledPast) {
-            sliderImage.classList.add('active')
-            sliderImage.classList.remove('deActive')
+    console.log(window.innerHeight)
+    arrows.forEach((arrow) => {
+        var rect = arrow.getBoundingClientRect(); 
+        var path = arrow.querySelector("path");
+        var halfWindow = window.innerHeight / 2
+        var length = path.getTotalLength();
+        
+        // console.log(window.innerHeight / 2)
+        console.log(rect.top)
+        // console.log(length)
+        console.log(rect.top / halfWindow)
+        
+        if (halfWindow > rect.top) {
+            path.style.strokeDashoffset = 0
+        } else if ((window.innerHeight * .75 ) < rect.top) {
+            path.style.strokeDashoffset = length;
         } else {
-            // sliderImage.classList.remove('active')
-            // sliderImage.classList.add('deActive')
+            path.style.strokeDashoffset = (length * ((rect.top - halfWindow)) / (halfWindow));
+
         }
 
-        console.log(sliderImage.offsetTop);
+
+        // console.log(imageBottom);
     })
 }
 
@@ -53,3 +62,23 @@ function debounce(func, wait = 20, immediate = true) {
     };
 }
 
+
+
+
+// function checkSlide(e) {
+//     images.forEach(sliderImage => {
+
+//         // half visable from the bottom point of each image 
+//         const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+//         // the bottom of the image
+//         const imageBottom = sliderImage.offsetTop + sliderImage.height;
+//         const isHalfShown = slideInAt > sliderImage.offsetTop;
+//         const isNotScrolledPast = window.scrollY < imageBottom;
+//         if (isHalfShown && isNotScrolledPast) {
+//             sliderImage.classList.add('active')
+//         } else {
+//             sliderImage.classList.remove('active')
+//         }
+//         console.log(sliderImage.height);
+//     })
+// }
